f5b7dd3a4369160e30ce06e963a6467cba489d88
Anonymous Indonesia <muhtaufiqmukhtar@gmail.com>
2025-05-24 17:45:06 +0800

    '20250524'

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "725977324"
+    serviceWorkerVersion: "3976787795"
   }
 });
